47fd622a2a81e8e54aedd540ea97e54c7c11fcd1
Guillermo del Fresno <gdelfresno@gmail.com>
2012-12-27 17:01:31 +0100

    Check new articles and notifies every 5 mins

--- Notipy/AppDelegate.py
+++ Notipy/AppDelegate.py
@@ -11,9 +11,29 @@ from Foundation import *
 from AppKit import *
 from objc import YES, NO, IBAction, IBOutlet
 import gntp.notifier
+from threading import Timer
 
-from libgreader import GoogleReader, ClientAuthMethod
+from libgreader import GoogleReader, ClientAuthMethod, ReaderUrl
 from PyObjCTools import NibClassBuilder
+class RepeatingTimer(object):
+	def __init__(self,interval, function, *args, **kwargs):
+		super(RepeatingTimer, self).__init__()
+		self.args = args
+		self.kwargs = kwargs
+		self.function = function
+		self.interval = interval
+    
+	def start(self):
+		self.callback()
+    
+	def stop(self):
+		self.interval = False
+    
+	def callback(self):
+		if self.interval:
+			self.function(*self.args, **self.kwargs)
+			Timer(self.interval, self.callback, ).start()
+
 
 class AppDelegate(NSObject):
     statusMenu = IBOutlet()
@@ -26,24 +46,56 @@ class AppDelegate(NSObject):
     user = None
     password = None
     
+    lastId = None
+    growl = None
+    
     def applicationDidFinishLaunching_(self, sender):
-        NSLog("Application did finish launching.")
+        appName = "Notipy" #bundle.localizedStringForKey_value_table_(kCFBundleNameKey,'Notipy',None)
+        self.growl = gntp.notifier.GrowlNotifier(
+                                                 applicationName = appName,
+                                                 notifications = ["New Article","Configuration Missing"],
+                                                 defaultNotifications = ["New Article"],
+                                                 )
+        try:
+            self.growl.register()
+        except:
+            print "Error registering Growl"
 
-    def testReader(self,readerUser, readerPassword):
+        if (self.user is None or self.password is None):
+            self.growl.notify(
+                              noteType = "Configuration Missing",
+                              title = "Configuration missing",
+                              description = "Google Reader Account not configured",
+                              sticky = False,
+                              priority = 1,
+                              )
+            self.prefWindow.makeKeyAndOrderFront_(None)
         
-        ca = ClientAuthMethod(readerUser,readerPassword)
-        reader = GoogleReader(ca)
-        if reader.buildSubscriptionList():
-            NSLog("Google Reader connect ok")
-        else:
-            NSLog("Error retrieving subscriptions from Google Reader")
-            return False
         
-        NSLog("Getting Google Reader categories")
-        categories = reader.getCategories()
-        NSLog("%d categories",len(categories))
-        for category in categories:
-            NSLog(category.label)
+        timer = RepeatingTimer(60.0*5,self.checkItems_,None)
+        timer.start()
+        NSLog("Application did finish launching.")
+    
+    def notifyNewItem(self,item):
+        if item is None:
+            return
+        
+        url = None
+        if 'canonical' in item.data:
+            url = item.data['canonical'][0]['href']
+        elif 'alternate' in item.data:
+            url = item.data['alternate'][0]['href']
+                
+        self.growl.notify(
+            noteType = "New Article",
+            title = item.origin['title'] + " published",
+            description = item.title,
+            sticky = False,
+            priority = 1,
+            callback = url
+        )
+
+        
 
     @IBAction
     def savePreferences_(self, sender):
@@ -54,15 +106,37 @@ class AppDelegate(NSObject):
     
     @IBAction
     def checkItems_(self, sender):
-        gntp.notifier.mini("Checking items")
 
-        self.testReader(self.user,self.password)
-                        
+        NSLog("Checking items")
+        if self.user is None or self.password is None:
+            return
+        
+        #Retrieve all items
+        ca = ClientAuthMethod(self.user,self.password)
+        reader = GoogleReader(ca)
+        reader.makeSpecialFeeds()
+        specials = reader.getSpecialFeed(ReaderUrl.READING_LIST)
+        specials.loadItems()
+        
+        if self.lastId is None:
+            item = specials.items[0]
 
+            self.lastId = item.id
+            self.notifyNewItem(item)
+        else:
+            if len(specials.items) > 0:
+                lastItem = specials.items[0].id
+                for item in specials.items:
+                    if item.id == self.lastId:
+                        break
+                    self.notifyNewItem(item)
+                self.lastId = lastItem
+        NSLog("Finished Checking items")
 
     def awakeFromNib(self):
         bundle = NSBundle.mainBundle()
         
+
         statusbar = NSStatusBar.systemStatusBar()
         # Create the statusbar item
         self.statusitem = statusbar.statusItemWithLength_(NSVariableStatusItemLength)
@@ -80,7 +154,6 @@ class AppDelegate(NSObject):
         self.statusitem.setMenu_(self.statusMenu)
         
         
-        if (self.user is None or self.password is None):
-            self.prefWindow.makeKeyAndOrderFront_(None)
         
-        print "awake"
+    
+    
